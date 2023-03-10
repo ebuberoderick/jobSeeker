8578feefa748453022cb1c02c6d4695e22655482
ebuberoderick <ebuberoderick2@gmail.com>
2023-03-11 00:37:42 +0100

    added database file

---
+++ d_b.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 11, 2023 at 12:34 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jobseeker`
+--
+CREATE DATABASE IF NOT EXISTS `jobseeker` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `jobseeker`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `applicants`
+--
+
+CREATE TABLE `applicants` (
+  `id` int(255) NOT NULL,
+  `email` varchar(233) NOT NULL,
+  `Job_id` int(253) NOT NULL,
+  `cv` varchar(255) NOT NULL,
+  `Full_name` varchar(233) NOT NULL,
+  `phone_number` varchar(122) NOT NULL,
+  `job_status` varchar(32) NOT NULL,
+  `data_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `id` int(234) NOT NULL,
+  `job_description` longtext NOT NULL,
+  `job_location` varchar(255) NOT NULL,
+  `salary_range` varchar(122) NOT NULL,
+  `job_title` text NOT NULL,
+  `date_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_description`
+--
+
+CREATE TABLE `job_description` (
+  `id` int(223) NOT NULL,
+  `name` varchar(231) NOT NULL,
+  `datatime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `job_description`
+--
+
+INSERT INTO `job_description` (`id`, `name`, `datatime`) VALUES
+(1, 'teacher', '2023-03-10 11:17:44'),
+(2, 'receptionist', '2023-03-10 11:18:19'),
+(3, 'care giver', '2023-03-10 11:18:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(253) NOT NULL,
+  `firstname` varchar(112) NOT NULL,
+  `lastname` varchar(220) NOT NULL,
+  `email` varchar(225) NOT NULL,
+  `phone_number` varchar(200) NOT NULL,
+  `user_level` varchar(30) NOT NULL,
+  `job_title` varchar(23) NOT NULL,
+  `office_location` varchar(234) NOT NULL,
+  `salary` varchar(23) NOT NULL,
+  `onPayroll` varchar(23) NOT NULL DEFAULT '0',
+  `user_password` varchar(255) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `phone_number`, `user_level`, `job_title`, `office_location`, `salary`, `onPayroll`, `user_password`, `datetime`) VALUES
+(1, 'admin', 'admin', 'admin@admin.com', '08080808080', 'admin', 'admin', '', '', '0', '', '2023-03-11 00:31:57');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `applicants`
+--
+ALTER TABLE `applicants`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `job_description`
+--
+ALTER TABLE `job_description`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `applicants`
+--
+ALTER TABLE `applicants`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `id` int(234) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `job_description`
+--
+ALTER TABLE `job_description`
+  MODIFY `id` int(223) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(253) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
